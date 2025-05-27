66cbec2e1e1ae088e06a28a1739b90477db53f39
rahulpihub <rahul.p.ihub@snsgroups.com>
2025-05-27 15:22:46 +0530

    The Quick Notes project completed

---
+++ public/config.js
@@ -0,0 +1,9 @@
+// Firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAq8qrXM0lw6c2OyUhV2oMpqpaLq1BA3SA",
+  authDomain: "quicknotesapp-8119c.firebaseapp.com",
+  projectId: "quicknotesapp-8119c",
+  storageBucket: "quicknotesapp-8119c.firebasestorage.app",
+  messagingSenderId: "287276885660",
+  appId: "1:287276885660:web:0e6d3560a9d989a5a0ef59"
+};
